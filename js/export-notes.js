227d4831212a3faea5fe53972eed8bc9693fe134
Ashley Porter <porter.ashley13@gmail.com>
2021-05-11 17:03:10 -0700

    add ability for export to detect which notes should be pulled from the grid

--- js/export-notes.js
+++ js/export-notes.js
@@ -1,8 +1,31 @@
+function reverseDigitSwitch(digit) {
+  switch (digit) {
+    case "zero":
+      digit = 0;
+      break;
+    case "one":
+      digit = 1;
+      break;
+    case "two":
+      digit = 2;
+      break;
+    case "three":
+      digit = 3;
+      break;
+    default:
+      break;
+  }
+  return digit;
+}
+
 $(document).ready(function () {
   $("button#export-data").click(function () {
     document.querySelectorAll('*').forEach(function (node) {
-      if (node.className) {
-        alert(node.className);
+      if (node.classList.contains("blue-style") || node.classList.contains("bomb-style") || node.classList.contains("red-style")) {
+        const coords = node.id.split("-");
+        row = reverseDigitSwitch(coords[0]);
+        col = reverseDigitSwitch(coords[1]);
+        alert(`${row}, ${col}`);
       }
     });
   });
