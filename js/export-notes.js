59b5711032937e60c3c645e79841cb34a700a522
Ashley Porter <porter.ashley13@gmail.com>
2021-05-14 13:46:51 -0700

    fix bug where values would not properly insert into note validation string

--- js/export-notes.js
+++ js/export-notes.js
@@ -20,25 +20,40 @@ function reverseDigitSwitch(digit) {
 
 $(document).ready(function () {
   $("button#export-data").click(function () {
-    document.querySelectorAll('*').forEach(function (node) {
-      if (node.classList.contains("blue-style") || node.classList.contains("bomb-style") || node.classList.contains("red-style")) {
-        const coords = node.id.split("-");
-        beat = $("#debug-note-beat").val();
-        // color = $("#debug-note-color").val();
-        color = parseInt(document.getElementById("debug-note-color").value);
-        angle = $("#debug-note-row").val();
-        row = reverseDigitSwitch(coords[0]);
-        col = reverseDigitSwitch(coords[1]);
-        // alert(`${row}, ${col}`);
-        // if (beat && color && angle && row && col) {
-          alert(`${beat}, ${color}, ${angle}, ${row}, ${col}`);
-          $("#_time_data").text(beat);
-          $("#_lineIndex_data").text(color);
-          $("#_lineLayer_data").text(angle);
-          $("#_type_data").text(row);
-          $("#_cutDirection_data").text(col);
-        // }
-      }
-    });
+    beat = $("#debug-note-beat").val();
+    color = $("#debug-note-color").text();
+    angle = $("#debug-note-row").text();
+    row = $("#debug-note-row").text();
+    col = $("#debug-note-col").text();
+
+    alert(`${beat}, ${color}, ${angle}, ${row}, ${col}`);
+    $("#_time_data").text(beat);
+    $("#_lineIndex_data").text(color);
+    $("#_lineLayer_data").text(angle);
+    $("#_type_data").text(row);
+    $("#_cutDirection_data").text(col);
+
+    
+    
+    // document.querySelectorAll('*').forEach(function (node) {
+      //   if (node.classList.contains("blue-style") || node.classList.contains("bomb-style") || node.classList.contains("red-style")) {
+        //     const coords = node.id.split("-");
+        //     beat = $("#debug-note-beat").val();
+        //     // color = $("#debug-note-color").val();
+        //     color = parseInt(document.getElementById("debug-note-color").value);
+        //     angle = $("#debug-note-row").val();
+        //     row = reverseDigitSwitch(coords[0]);
+        //     col = reverseDigitSwitch(coords[1]);
+        //     // alert(`${row}, ${col}`);
+        //     // if (beat && color && angle && row && col) {
+      //       alert(`${beat}, ${color}, ${angle}, ${row}, ${col}`);
+    //       $("#_time_data").text(beat);
+    //       $("#_lineIndex_data").text(color);
+    //       $("#_lineLayer_data").text(angle);
+    //       $("#_type_data").text(row);
+    //       $("#_cutDirection_data").text(col);
+    //     // }
+    //   }
+    // });
   });
 });
