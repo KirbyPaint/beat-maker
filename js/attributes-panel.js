80c0702b967b1351ce503a724e9e7d325f042c5c
Ashley Porter <porter.ashley13@gmail.com>
2021-05-14 11:50:37 -0700

    add most info.dat data successfully to export area of page

--- js/attributes-panel.js
+++ js/attributes-panel.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
   // info.dat 
   $("button#export-data-button").click(function () {
-    const versionData = $("#_version").val();
-    $("#_version_data").append(versionData);
-    alert(versionData);
+    $("#_version_data").text($("#_version").val());
+    $("#_songName_data").text($("#_songName").val());
+    $("#_songSubName_data").text($("#_songSubName").val());
+    $("#_songAuthorName_data").text($("#_songAuthorName").val());
+    $("#_levelAuthorName_data").text($("#_levelAuthorName").val());
+    $("#_beatsPerMinute_data").text($("#_beatsPerMinute").val());
+    $("#_songTimeOffset_data").text($("#_songTimeOffset").val());
+    $("#_shuffle_data").text($("#_shuffle").val());
+    $("#_shufflePeriod_data").text($("#_shufflePeriod").val());
+    $("#_previewStartTime_data").text($("#_previewStartTime").val());
+    $("#_previewDuration_data").text($("#_previewDuration").val());
+    $("#_songFilename_data").text($("#_songFilename").val());
+    $("#_coverImageFilename_data").text($("#_coverImageFilename").val());
+    $("#_environmentName_data").text($("#_environmentName").val());
+    $("#_allDirectionsEnvironmentName_data").text($("#_allDirectionsEnvironmentName").val());
   });
 });
