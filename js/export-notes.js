62e0f976a6dbd4086c946527fdfb7af1d611a64f
Ashley Porter <porter.ashley13@gmail.com>
2021-05-14 13:24:37 -0700

    partially add single note data to intended note chart... having issues

--- js/export-notes.js
+++ js/export-notes.js
@@ -23,9 +23,21 @@ $(document).ready(function () {
     document.querySelectorAll('*').forEach(function (node) {
       if (node.classList.contains("blue-style") || node.classList.contains("bomb-style") || node.classList.contains("red-style")) {
         const coords = node.id.split("-");
+        beat = $("#debug-note-beat").val();
+        // color = $("#debug-note-color").val();
+        color = parseInt(document.getElementById("debug-note-color").value);
+        angle = $("#debug-note-row").val();
         row = reverseDigitSwitch(coords[0]);
         col = reverseDigitSwitch(coords[1]);
-        alert(`${row}, ${col}`);
+        // alert(`${row}, ${col}`);
+        // if (beat && color && angle && row && col) {
+          alert(`${beat}, ${color}, ${angle}, ${row}, ${col}`);
+          $("#_time_data").text(beat);
+          $("#_lineIndex_data").text(color);
+          $("#_lineLayer_data").text(angle);
+          $("#_type_data").text(row);
+          $("#_cutDirection_data").text(col);
+        // }
       }
     });
   });
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import Beat from './Beat.js';
+
+// What I want this to do is to save the beat, containing each note in that beat, as an array or something with exportable data
+function saveBeat() {
+}
